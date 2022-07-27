3b6246ff7c26fa1f2b36ba69cfb84bee61e7849a
mistake2000-Git <18521002@gm.uit.edu.vn>
2022-07-27 11:08:48 +0700

    Sign in,Sign Up for user.

--- src/main/java/com/ecommerce/ecommerceapp/exceptions/ExceptionControlerAdvice.java
+++ src/main/java/com/ecommerce/ecommerceapp/exceptions/ExceptionControlerAdvice.java
@@ -1,4 +1,16 @@
 package com.ecommerce.ecommerceapp.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ExceptionControlerAdvice {
+    @ExceptionHandler(value = CustomException.class)
+    public final ResponseEntity<String> handleUpdateFailException(CustomException exception)
+    {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
