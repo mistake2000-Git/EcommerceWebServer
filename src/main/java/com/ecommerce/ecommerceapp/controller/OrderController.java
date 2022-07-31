75ab6b23aa461a9d9099ebf84a058a23f1794a74
mistake2000-Git <18521002@gm.uit.edu.vn>
2022-07-31 10:24:36 +0700

    Stripe integration

--- src/main/java/com/ecommerce/ecommerceapp/controller/OrderController.java
+++ src/main/java/com/ecommerce/ecommerceapp/controller/OrderController.java
@@ -1,11 +1,43 @@
 package com.ecommerce.ecommerceapp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.ecommerceapp.common.ApiResponse;
+import com.ecommerce.ecommerceapp.exceptions.AuthenticationFailException;
+import com.ecommerce.ecommerceapp.exceptions.CartItemDoesNotExistException;
+import com.ecommerce.ecommerceapp.model.Order;
+import com.ecommerce.ecommerceapp.model.User;
+import com.ecommerce.ecommerceapp.service.AuthenticationService;
+import com.ecommerce.ecommerceapp.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    AuthenticationService authenticationService;
+    @Autowired
+    OrderService orderService;
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> placeOrder(@RequestParam("token") String token,@RequestParam("sessionId") String sessionId)
+            throws AuthenticationFailException, CartItemDoesNotExistException {
+        authenticationService.authenticate(token);
+        User user = authenticationService.getTokenUser(token);
+        orderService.placeOrder(user,sessionId);
+        return new ResponseEntity<>(new ApiResponse(true,"Order has been placed"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllOrderS")
+    public ResponseEntity<List<Order>> listOrderOfUser(@RequestParam("token") String token) throws AuthenticationFailException{
+        authenticationService.authenticate(token);
+        User user = authenticationService.getTokenUser(token);
+        List<Order>  orders = orderService.listUserOrders(user);
+        return  new ResponseEntity<>(orders,HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/ecommerce/ecommerceapp/service/OrderService.java
+++ src/main/java/com/ecommerce/ecommerceapp/service/OrderService.java
@@ -1,6 +1,15 @@
 package com.ecommerce.ecommerceapp.service;
 
+import com.ecommerce.ecommerceapp.dto.cart.CartDto;
+import com.ecommerce.ecommerceapp.dto.cart.CartItemDto;
 import com.ecommerce.ecommerceapp.dto.checkout.CheckOutItemDto;
+import com.ecommerce.ecommerceapp.exceptions.AuthenticationFailException;
+import com.ecommerce.ecommerceapp.exceptions.CartItemDoesNotExistException;
+import com.ecommerce.ecommerceapp.model.Order;
+import com.ecommerce.ecommerceapp.model.OrderItem;
+import com.ecommerce.ecommerceapp.model.User;
+import com.ecommerce.ecommerceapp.repository.OrderItemRepository;
+import com.ecommerce.ecommerceapp.repository.OrderRepository;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
@@ -8,9 +17,12 @@ import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,7 +32,10 @@ public class OrderService {
 
     @Autowired
     private CartService cartService;
-
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
     @Value("${BASE_URL}")
     private String baseURL;
 
@@ -76,4 +91,36 @@ public class OrderService {
                 .build();
         return Session.create(params);
     }
+    public void placeOrder(User user, String sessionId) throws CartItemDoesNotExistException
+    {
+        CartDto cartDto = cartService.readCart(user);
+        List<CartItemDto> cartItemDtoList = cartDto.getCartItemDtoList();
+
+        // create the order and save it
+        Order newOrder = new Order();
+        newOrder.setCreatedDate(new Date());
+        newOrder.setSessionId(sessionId);
+        newOrder.setUser(user);
+        newOrder.setTotalPrice(cartDto.getTotalCost());
+        orderRepository.save(newOrder);
+
+        for (CartItemDto cartItemDto : cartItemDtoList) {
+            // create orderItem and save each one
+            OrderItem orderItem = new OrderItem();
+            orderItem.setCreatedDate(new Date());
+            orderItem.setPrice(cartItemDto.getProduct().getPrice());
+            orderItem.setProduct(cartItemDto.getProduct());
+            orderItem.setQuantity(cartItemDto.getQuantity());
+            orderItem.setOrder(newOrder);
+            // add to order item list
+            orderItemRepository.save(orderItem);
+        }
+
+    }
+    @GetMapping("/getUserOrders")
+    public List<Order> listUserOrders(User user)
+    {
+        return orderItemRepository.findAllByUserOrderByCreatedDateDesc(user);
+    }
+
 }
